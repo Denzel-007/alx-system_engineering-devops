adccaffbc667df90615f75dd7cd5cf1d664271f5
Denzel-007 <akinlaluredaniel@gmail.com>
2023-06-23 08:03:11 -0700

    Updated

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,6 @@
 # create a file in /tmp
-file {'/tmp/holberton':
-  path    => '/tmp/holberton',
+file {'/tmp/school':
+  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
